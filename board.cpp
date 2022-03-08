2924749444a031e6d0ab611d47284f7502ff74fc
gunnhamm <ghammon5@u.rochester.edu>
2022-03-07 23:09:47 -0500

    Board functionality

--- board.cpp
+++ board.cpp
@@ -1,43 +1,67 @@
 #include "board.hpp"
-
-Board::Board() {
-    colors = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0},
-        {0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-        {0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0},
-        {0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
-
-    pieces = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0},
-        {0, 0, 5, 6, 7, 8, 9, 7, 6, 5, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
-        {0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0},
-        {0, 0, 5, 6, 7, 8, 9, 7, 6, 5, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
-
-
-
-
-
+#include <iostream>
+void Board::print_board() {
+    for (int i = 2; i < 10; ++i) {
+        for (int j = 2; j < 10; ++j) {
+            char piece = '-';
+            switch (pieces[i][j]) {
+                case pawn:
+                    piece = 'p';
+                    break;
+                case rook:
+                    piece = 'r';
+                    break;
+                case knight:
+                    piece = 'n';
+                    break;
+                case bishop:
+                    piece = 'b';
+                    break;
+                case queen:
+                    piece = 'q';
+                    break;
+                case king:
+                    piece = 'k';
+            }
+            if (piece != '-' && colors[i][j] == white) {
+                piece -= 32;
+            }
+            std::cout << piece << ' ';
+        }
+        std::cout << std::endl;
     }
+}
+std::vector<Move> Board::gen_moves() {
+    
+    for (int i = 2; i < 10; ++i) {
+        for (int j = 2; j < 10; ++j) {
+            if (colors[i][j] == white) {
+                switch (pieces[i][j]) {
+                    case pawn:
+                        piece = 'p';
+                        break;
+                    case rook:
+                        piece = 'r';
+                        break;
+                    case knight:
+                        piece = 'n';
+                        break;
+                    case bishop:
+                        piece = 'b';
+                        break;
+                    case queen:
+                        piece = 'q';
+                        break;
+                    case king:
+                        piece = 'k';
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    Board b;
+    b.print_board();
+    return 0;
 }
--- board.hpp
+++ board.hpp
@@ -1,4 +1,5 @@
 #include "move.hpp"
+#include <vector>
 const int null = 0;
 const int blank = 1;
 const int black = 2;
@@ -13,15 +14,44 @@ const int king = 9;
 
 class Board {
     public:
-        Move pawn_moves();
-        Move rook_moves();
-        Move knight_moves();
-        Move bishop_moves();
-        Move queen_moves();
-        Move king_moves();
+        std::vector<Move> gen_moves();
+        inline void pawn_moves(std::vector<Move> move_list);
+        inline void rook_moves(std::vector<Move> move_list);
+        inline void knight_moves(std::vector<Move> move_list);
+        inline void bishop_moves(std::vector<Move> move_list);
+        inline void queen_moves(std::vector<Move> move_list);
+        inline void king_moves(std::vector<Move> move_list);
+        void print_board();
 
 
     private:
-        int colors[12][12];
-        int pieces[12][12];
+        int colors[12][12] = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0},
+            {0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0},
+            {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+            {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+            {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+            {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+            {0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0},
+            {0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        int pieces[12][12] = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 5, 6, 7, 8, 9, 7, 6, 5, 0, 0},
+            {0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0},
+            {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+            {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+            {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+            {0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0},
+            {0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 0, 0},
+            {0, 0, 5, 6, 7, 8, 9, 7, 6, 5, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
 };
