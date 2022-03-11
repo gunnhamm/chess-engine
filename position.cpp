c31ddd606fe811bf14ee427167100faa8ed7cdee
gunnhamm <ghammon5@u.rochester.edu>
2022-03-11 14:40:53 -0500

    Pawn, Knight moves

--- position.cpp
+++ position.cpp
@@ -5,16 +5,26 @@
 std::shared_ptr<std::vector<Move>> Position::gen_moves(int row, int col) {
     std::shared_ptr<std::vector<Move>> move_list(new std::vector<Move>());
     for (int i = row; i < board_end; ++i) {
-        for (int i = col; i < board_end; ++i) {
+        for (int j = col; j < board_end; ++j) {
             if (board->colors[i][j] == white) {
-                piece_moves(move_list, i, j);
+                switch (board->pieces[i][j]) {
+                    case pawn:
+                        pawn_moves(move_list, i, j);
+                    case knight:
+                        knight_moves(move_list, i, j);
+                    case king:
+                        king_moves(move_list, i, j);
+                    default:
+                        sliding_moves(move_list, i, j);
+                }
                 return move_list; 
             }
         }
     }
+    return move_list; 
 }
 
-inline void Position::piece_moves(std::shared_ptr<std::vector<Move>> move_list, int row, int col) {
+void Position::sliding_moves(std::shared_ptr<std::vector<Move>> move_list, int row, int col) {
     const int piece_type = board->pieces[row][col];
     Moveset possible_moves = movesets.at(piece_type);
     int x_distance, y_distance;
@@ -32,4 +42,40 @@ inline void Position::piece_moves(std::shared_ptr<std::vector<Move>> move_list, 
         }
     }
 
+}
+void Position::pawn_moves(std::shared_ptr<std::vector<Move>> move_list, int row, int col) {
+    if (board->pieces[row - 1][col] == blank) {
+        move_list->push_back(Move(row, col, row - 1, col));
+        if (board->pieces[row - 2][col] == blank) {
+            move_list->push_back(Move(row, col, row - 2, col));
+        }
+    }
+    int capture_left = board->pieces[row - 1][col - 1];
+    int capture_right = board->pieces[row - 1][col + 1];
+    bool en_passant_right = en_passant_col == col + 1;
+    bool en_passant_left = en_passant_col == col - 1;
+
+    if (capture_left == black || (capture_left == blank && en_passant_left)) {
+        move_list->push_back(Move(row, col, row - 1, col - 1));
+    }
+    if (capture_right = black || capture_left == blank && en_passant_right) {
+        move_list->push_back(Move(row, col, row - 1, col + 1));
+    }
+}
+void Position::knight_moves(std::shared_ptr<std::vector<Move>> move_list, int row, int col) {
+    Moveset knight_moves = movesets.at(knight);
+    const int* x_moves = knight_moves.x_moves;
+    const int* y_moves = knight_moves.y_moves;
+
+    for (int i = 0; i < 8; i++) {
+        int move_x = col + x_moves[i];
+        int move_y = row + y_moves[i];
+
+        if (board->colors[move_y][move_x] == black || board->colors[move_y][move_x] == blank) {
+            move_list->push_back(Move(row, col, move_y, move_x));
+        }
+    }
+}
+void Position::king_moves(std::shared_ptr<std::vector<Move>> move_list, int row, int col) {
+    
 }
--- position.hpp
+++ position.hpp
@@ -3,7 +3,13 @@
 class Position {
     public:
         std::shared_ptr<std::vector<Move>> gen_moves(int row, int col);
-        inline void piece_moves(std::shared_ptr<std::vector<Move>> move_list, int row, int col);
+        void sliding_moves(std::shared_ptr<std::vector<Move>> move_list, int row, int col);
+        void pawn_moves(std::shared_ptr<std::vector<Move>> move_list, int row, int col);
+        void knight_moves(std::shared_ptr<std::vector<Move>> move_list, int row, int col);
+        void king_moves(std::shared_ptr<std::vector<Move>> move_list, int row, int col);
+
+
+
 
         
     private:
@@ -16,6 +22,7 @@ class Position {
         bool bk_castle;
         bool bq_castle;
 
-        bool en_passant;
+        int en_passant_col;
+
         bool king_passant;
 };
